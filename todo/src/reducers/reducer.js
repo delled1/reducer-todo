9343c23a31c547e341e76eecc0ea18decf52d623
Eric Della <ericdella13@gmail.com>
2020-12-16 15:37:56 -0800

    setup reducers folder, reducer.js file

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,17 @@
+
+
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: ""
+}
+
+const toDoReducer = (state, action) => {
+    switch(action.type) {
+        default: return state;
+        
+    }
+}
+
+
+export default toDoReducer
